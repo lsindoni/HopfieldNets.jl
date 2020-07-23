9b04d41dd4b5f95339cb8c54d98ee73fb58dc6df
lsindoni <lorenzo.sindoni@gmail.com>
2020-07-23 11:08:35 +0200

    import @printf

--- src/HopfieldNets.jl
+++ src/HopfieldNets.jl
@@ -2,6 +2,8 @@ module HopfieldNets
     export HopfieldNet, DiscreteHopfieldNet, ContinuousHopfieldNet
     export update!, energy, settle!, train!, associate!, storkeytrain!
 
+    import Printf: @printf
+
     include("generic.jl")
     include("discrete.jl")
     include("continuous.jl")
